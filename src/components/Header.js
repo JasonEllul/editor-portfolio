b05513f7038c3a1d3efbab8f60b0fba945b3aa17
jasonellul <jason.j.ellul@gmail.com>
2019-08-21 14:37:36 -0400

    embed

--- src/components/Header.js
+++ src/components/Header.js
@@ -44,7 +44,7 @@ class Header extends Component {
     return (
       <div className='navbar-wrapper' style={{
         opacity: logoHidden ? 1 : 0,
-        background: scroll ? '#0C192A' : 'linear-gradient(#0C192A, transparent)',
+        backgroundColor: scroll ? '#0C192A' : 'transparent',
         boxShadow: scroll ? '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)' : 'none'
       }}>
         <img className='navbar-brand' src={logo} />
--- src/containers/Instapage.js
+++ src/containers/Instapage.js
@@ -8,6 +8,7 @@ class Instapage extends Component {
 
   constructor(props) {
     super(props);
+    this.instagramUrls = ['https://instagr.am/p/BzZkd3BlDnM/', ''];
   }
 
   componentDidMount() {
@@ -15,27 +16,75 @@ class Instapage extends Component {
   }
 
 
+
+
   render() {
     return (
-      <div className='instapage-wrapper' style={{ backgroundColor: '#ffffff' }}>
-        <h1 className='instapage-title'>MY PORTFOLIO</h1>
-        <div className='instapage-row'>
-          <InstagramEmbed
-            url='https://instagr.am/p/BzZkd3BlDnM/'
-            maxWidth={320}
-            hideCaption={false}
-            containerTagName='div'
-            protocol=''
-            injectScript
-            onLoading={() => { }}
-            onSuccess={() => { }}
-            onAfterRender={() => { }}
-            onFailure={() => { }}
-          />
-          <div className='instapage-row-text-wrapper'>
-            <p className='instapage-row-title'>Von Miller</p>
-            <p className='instapage-row-text'>All generated information from the Internet, books, encyclopedia, etc., you can generate many random lists to meet your use. All the lists are random, so each time the results are not the same. The generators contain addresses, animals, music, creatures, celebrities, sports, geography, food, architecture and so on.</p>
+      <div className='instapage-wrapper' style={{ backgroundColor: '#fafafa' }}>
+        <div className='instapage-showcase-wrapper'>
+          <h1 className='instapage-title'>RECENT FOOTBALL EDITS</h1>
+          <div className='instapage-showcase'>
+            <div className='embed-wrapper'>
+              <InstagramEmbed
+                url='https://www.instagram.com/p/BwF_LHHF4lP/'
+                maxWidth={320}
+                hideCaption={false}
+                containerTagName='div'
+                protocol=''
+                injectScript
+                onLoading={() => { }}
+                onSuccess={() => { }}
+                onAfterRender={() => { }}
+                onFailure={() => { }}
+              />
+            </div>
+            <div className='embed-wrapper'>
+              <InstagramEmbed
+                url='https://instagr.am/p/BzZkd3BlDnM/'
+                maxWidth={320}
+                hideCaption={false}
+                containerTagName='div'
+                protocol=''
+                injectScript
+                onLoading={() => { }}
+                onSuccess={() => { }}
+                onAfterRender={() => { }}
+                onFailure={() => { }}
+              />
+            </div>
+            <div className='embed-wrapper'>
+              <InstagramEmbed
+                url='https://www.instagram.com/p/B0rwmBRgvjI/'
+                maxWidth={320}
+                hideCaption={false}
+                containerTagName='div'
+                protocol=''
+                injectScript
+                onLoading={() => { }}
+                onSuccess={() => { }}
+                onAfterRender={() => { }}
+                onFailure={() => { }}
+              />
+            </div>
+            <div className='embed-wrapper'>
+              <InstagramEmbed
+                url='https://www.instagram.com/p/B0TkaAaBLj4/'
+                maxWidth={320}
+                hideCaption={false}
+                containerTagName='div'
+                protocol=''
+                injectScript
+                onLoading={() => { }}
+                onSuccess={() => { }}
+                onAfterRender={() => { }}
+                onFailure={() => { }}
+              />
+            </div>
           </div>
+        </div>
+        <div className='instapage-row-text-wrapper'>
+          <p className='instapage-row-title'>Von Miller</p>
+          <p className='instapage-row-text'>Show transparent background picture of player under the instragram embed on the right. The rest of the screen looks like Profilepage on personal portfolio.</p>
         </div>
       </div>
     )
