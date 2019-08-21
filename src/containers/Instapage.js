5954109834aa1b1faca42b381009d930cb00b236
jasonellul <jason.j.ellul@gmail.com>
2019-08-21 15:32:46 -0400

    last

--- src/containers/Instapage.js
+++ src/containers/Instapage.js
@@ -25,7 +25,7 @@ class Instapage extends Component {
       <div className='instapage-wrapper' style={{ backgroundColor: '#fafafa' }}>
         <SocialsWidget />
         <div className='instapage-showcase-wrapper'>
-          <h1 className='instapage-title'>RECENT FOOTBALL EDITS</h1>
+          <h1 className='instapage-title'>RECENT VIDEO EDITS</h1>
           <div className='instapage-showcase'>
             <div className='embed-wrapper'>
               <InstagramEmbed
@@ -64,6 +64,48 @@ class Instapage extends Component {
             </div>
           </div>
         </div>
+        <div className='instapage-showcase-wrapper'>
+          <h1 className='instapage-title'>RECENT ANIMATED PICTURES</h1>
+          <div className='instapage-showcase'>
+            <div className='embed-wrapper'>
+              <InstagramEmbed
+                url='https://www.instagram.com/p/BwF_LHHF4lP/'
+                maxWidth={320}
+                hideCaption={false}
+                containerTagName='div'
+                protocol=''
+                injectScript
+                onLoading={() => { }}
+                onSuccess={() => { }}
+                onAfterRender={() => { }}
+                onFailure={() => { }}
+              />
+            </div>
+            <div className='embed-wrapper'>
+              <InstagramEmbed
+                url='https://instagr.am/p/BzZkd3BlDnM/'
+                maxWidth={320}
+                hideCaption={false}
+              />
+            </div>
+            <div className='embed-wrapper'>
+              <InstagramEmbed
+                url='https://www.instagram.com/p/B0rwmBRgvjI/'
+                maxWidth={320}
+                hideCaption={false}
+              />
+            </div>
+            <div className='embed-wrapper'>
+              <InstagramEmbed
+                url='https://www.instagram.com/p/B0TkaAaBLj4/'
+                maxWidth={320}
+                hideCaption={false}
+              />
+            </div>
+          </div>
+        </div>
+
+
         <div className='instapage-row-text-wrapper'>
           <p className='instapage-row-title'>Von Miller</p>
           <p className='instapage-row-text'>Show transparent background picture of player under the instragram embed on the right. The rest of the screen looks like Profilepage on personal portfolio.</p>
