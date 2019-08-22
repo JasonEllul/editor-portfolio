9dbe2242be6386123f97bbc7051cdec80400874c
jasonellul <jason.j.ellul@gmail.com>
2019-08-21 22:18:31 -0400

    Force Pushed All Files

--- src/containers/Instapage.js
+++ src/containers/Instapage.js
@@ -25,82 +25,83 @@ class Instapage extends Component {
       <div className='instapage-wrapper' style={{ backgroundColor: '#fafafa' }}>
         <SocialsWidget />
         <div className='instapage-showcase-wrapper'>
-          <h1 className='instapage-title'>RECENT VIDEO EDITS</h1>
           <div className='instapage-showcase'>
-            <div className='embed-wrapper'>
-              <InstagramEmbed
-                url='https://www.instagram.com/p/BwF_LHHF4lP/'
-                maxWidth={320}
-                hideCaption={false}
-                containerTagName='div'
-                protocol=''
-                injectScript
-                onLoading={() => { }}
-                onSuccess={() => { }}
-                onAfterRender={() => { }}
-                onFailure={() => { }}
-              />
-            </div>
-            <div className='embed-wrapper'>
-              <InstagramEmbed
-                url='https://instagr.am/p/BzZkd3BlDnM/'
-                maxWidth={320}
-                hideCaption={false}
-              />
-            </div>
-            <div className='embed-wrapper'>
-              <InstagramEmbed
-                url='https://www.instagram.com/p/B0rwmBRgvjI/'
-                maxWidth={320}
-                hideCaption={false}
-              />
-            </div>
-            <div className='embed-wrapper'>
-              <InstagramEmbed
-                url='https://www.instagram.com/p/B0TkaAaBLj4/'
-                maxWidth={320}
-                hideCaption={false}
-              />
+            <p className='instapage-title'>RECENT VIDEO EDITS</p>
+            <div className='embed-container'>
+              <div className='embed-wrapper'>
+                <InstagramEmbed
+                  url='https://www.instagram.com/p/BwF_LHHF4lP/'
+                  maxWidth={320}
+                  hideCaption={false}
+                  containerTagName='div'
+                  protocol=''
+                  injectScript
+                  onLoading={() => { }}
+                  onSuccess={() => { }}
+                  onAfterRender={() => { }}
+                  onFailure={() => { }}
+                />
+              </div>
+              <div className='embed-wrapper'>
+                <InstagramEmbed
+                  url='https://instagr.am/p/BzZkd3BlDnM/'
+                  maxWidth={320}
+                />
+              </div>
+              <div className='embed-wrapper'>
+                <InstagramEmbed
+                  url='https://www.instagram.com/p/B0rwmBRgvjI/'
+                  maxWidth={320}
+                />
+              </div>
+              <div className='embed-wrapper'>
+                <InstagramEmbed
+                  url='https://www.instagram.com/p/B0TkaAaBLj4/'
+                  maxWidth={320}
+                />
+              </div>
             </div>
           </div>
         </div>
         <div className='instapage-showcase-wrapper'>
-          <h1 className='instapage-title'>RECENT ANIMATED PICTURES</h1>
           <div className='instapage-showcase'>
-            <div className='embed-wrapper'>
-              <InstagramEmbed
-                url='https://www.instagram.com/p/BwF_LHHF4lP/'
-                maxWidth={320}
-                hideCaption={false}
-                containerTagName='div'
-                protocol=''
-                injectScript
-                onLoading={() => { }}
-                onSuccess={() => { }}
-                onAfterRender={() => { }}
-                onFailure={() => { }}
-              />
-            </div>
-            <div className='embed-wrapper'>
-              <InstagramEmbed
-                url='https://instagr.am/p/BzZkd3BlDnM/'
-                maxWidth={320}
-                hideCaption={false}
-              />
-            </div>
-            <div className='embed-wrapper'>
-              <InstagramEmbed
-                url='https://www.instagram.com/p/B0rwmBRgvjI/'
-                maxWidth={320}
-                hideCaption={false}
-              />
-            </div>
-            <div className='embed-wrapper'>
-              <InstagramEmbed
-                url='https://www.instagram.com/p/B0TkaAaBLj4/'
-                maxWidth={320}
-                hideCaption={false}
-              />
+            <h1 className='instapage-title'>RECENT ANIMATIONS</h1>
+            <div className='embed-container'>
+              <div className='embed-wrapper'>
+                <InstagramEmbed
+                  url='https://www.instagram.com/p/BwF_LHHF4lP/'
+                  maxWidth={320}
+                  hideCaption={false}
+                  containerTagName='div'
+                  protocol=''
+                  injectScript
+                  onLoading={() => { }}
+                  onSuccess={() => { }}
+                  onAfterRender={() => { }}
+                  onFailure={() => { }}
+                />
+              </div>
+              <div className='embed-wrapper'>
+                <InstagramEmbed
+                  url='https://instagr.am/p/BzZkd3BlDnM/'
+                  maxWidth={320}
+                  hideCaption={false}
+                />
+              </div>
+              <div className='embed-wrapper'>
+                <InstagramEmbed
+                  url='https://www.instagram.com/p/B0rwmBRgvjI/'
+                  maxWidth={320}
+                  hideCaption={false}
+                />
+              </div>
+              <div className='embed-wrapper'>
+                <InstagramEmbed
+                  url='https://www.instagram.com/p/B0TkaAaBLj4/'
+                  maxWidth={320}
+                  hideCaption={false}
+                />
+              </div>
             </div>
           </div>
         </div>
